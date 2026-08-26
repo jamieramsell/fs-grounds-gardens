592d626e664624ba893f31ac87655dc6eb909642
Jamie <jamieramsell07@outlook.com>
2026-08-26 19:49:24 +0100

    feat: add hollow stars to the end of a rating if it is not equal to 5

--- components/reviews/ReviewsGrid.tsx
+++ components/reviews/ReviewsGrid.tsx
@@ -14,6 +14,7 @@ function ReviewCard({ review }: ReviewCardProps) {
         className="flex"
         aria-label={`${review.rating} out of 5 stars`}
       >
+        {/* Filled stars represent the number of stars given */}
         {Array.from({ length: review.rating }, (_, i) => {
           return (
             <Star
@@ -21,6 +22,21 @@ function ReviewCard({ review }: ReviewCardProps) {
               className="mx-0.5"
               style={{ color: "#facc15" }}
               fill="currentColor"
+              size={30}
+              aria-hidden
+            />
+          );
+        })}
+
+        {/* Hollow stars represent the difference between the given review and
+         * the maximum (5)
+         */}
+        {Array.from({ length: 5 - review.rating }, (_, i) => {
+          return (
+            <Star
+              key={i}
+              className="mx-0.5"
+              style={{ color: "#facc15" }}
               size={30}
               aria-hidden
             />
