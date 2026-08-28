e5fc5057068495fe4b30715012fe1065fe7fbb97
Jamie <jamieramsell07@outlook.com>
2026-08-28 22:02:58 +0100

    feat: define the contact form

---
+++ components/ContactForm.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import { ChangeEvent, SubmitEvent, useState } from "react";
+
+export default function ContactForm() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+    methodOfContact: "",
+  });
+
+  function handleChange(
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>,
+  ) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e: SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(form);
+  }
+
+  const formLabelStyle = "block text-brand font-bold";
+  const formTextInputStyle =
+    "w-full border border-brand-accent text-ink/80 rounded p-2";
+  const formSubmitButtonStyle =
+    "inline-block rounded-full px-5 py-2.5 font-medium transition-colors bg-brand text-white hover:bg-brand-dark cursor-pointer";
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="max-w-md space-y-4">
+        <label htmlFor="name" className={formLabelStyle}>
+          Name
+        </label>
+        <input
+          id="name"
+          name="name"
+          className={formTextInputStyle}
+          value={form.name}
+          onChange={handleChange}
+        />
+        <label htmlFor="email" className={formLabelStyle}>
+          Email
+        </label>
+        <input
+          id="email"
+          name="email"
+          className={formTextInputStyle}
+          value={form.email}
+          onChange={handleChange}
+        />
+        <label htmlFor="phone" className={formLabelStyle}>
+          Phone
+        </label>
+        <input
+          id="phone"
+          name="phone"
+          className={formTextInputStyle}
+          value={form.phone}
+          onChange={handleChange}
+        />
+        <label htmlFor="message" className={formLabelStyle}>
+          Message
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          className={formTextInputStyle}
+          value={form.message}
+          onChange={handleChange}
+        />
+        <label htmlFor="methodOfContact" className={formLabelStyle}>
+          Preferred method of contact
+        </label>
+        <select
+          id="methodOfContact"
+          name="methodOfContact"
+          className={formTextInputStyle}
+          value={form.methodOfContact}
+          onChange={handleChange}
+        >
+          <option value="" disabled>
+            Choose a method of contact...
+          </option>
+          <option value="email">Email</option>
+          <option value="telephone">Telephone</option>
+          <option value="text">Text</option>
+        </select>
+        {form.methodOfContact === "email" && (
+          <p className="text-ink/80">
+            Please note that email responses may come from a personal email
+            address.
+          </p>
+        )}
+        <button type="submit" className={formSubmitButtonStyle}>
+          Submit
+        </button>
+      </form>
+    </>
+  );
+}
